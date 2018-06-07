7cbb1ffce79b45132489787b26128a0412bfb8a6
YanChunlin96 <1519780292@qq.com>
2018-06-07 10:03:11 +0800

    购买页

--- public/js/mall/mall.js
+++ public/js/mall/mall.js
@@ -8,3 +8,13 @@ $("#hot>li").on("click",function () {
     $("#hot>li").attr("class","");
     $(this).attr("class","active");
 });
+$(".listborder").mouseover(function () {
+    $(this).children(".introborderDiv").children("img").css("transform","scale(1.2)");
+    $(this).children(".introborder").children(".add_shop_cart").css("transform","translateY(-180px)");
+    $(".introborder").css("overflow","visible");
+});
+$(".listborder").mouseout(function () {
+    $(this).children(".introborderDiv").children("img").css("transform","scale(1)");
+    $(this).children(".introborder").children(".add_shop_cart").css("transform","translateY(0px)");
+    $(".introborder").css("overflow","hidden");
+});
