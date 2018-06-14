abb815161a394907a70f2d2252292b23194de086
Xiexinming <30278740+Xiexinming@users.noreply.github.com>
2018-06-14 14:51:29 +0800

    123

--- public/js/Micro_landscape/Micro_landscape.js
+++ public/js/Micro_landscape/Micro_landscape.js
@@ -4,7 +4,7 @@ window.onscroll=function () {
     if (t>20){
 
         $(".wz").css("marginLeft", "-1%");
-        $(".wz").css("transition", "all .5s linear");
+        $(".wz").css("transition", "all .8s linear");
         //Ô­Éújs
         // var banner=document.getElementById("banner");
         // banner.style.marginLeft="26%";
@@ -12,34 +12,34 @@ window.onscroll=function () {
     }
     if (t>800){
         $(".bz1").css("marginLeft", "-1%");
-        $(".bz1").css("transition", "all .5s linear");
+        $(".bz1").css("transition", "all .8s linear");
     }
     if (t>1000){
         $(".bz2").css("marginLeft", "-1%");
-        $(".bz2").css("transition", "all .5s linear");
+        $(".bz2").css("transition", "all .8s linear");
     }
     if (t>1200){
         $(".bz3").css("marginLeft", "-1%");
-        $(".bz3").css("transition", "all .5s linear");
+        $(".bz3").css("transition", "all .8s linear");
     }
     if (t>1500){
         $(".bz4").css("marginLeft", "-1%");
-        $(".bz4").css("transition", "all .5s linear");
+        $(".bz4").css("transition", "all .8s linear");
     }
     if (t>1800){
         $(".bz5").css("marginLeft", "-1%");
-        $(".bz5").css("transition", "all .5s linear");
+        $(".bz5").css("transition", "all .8s linear");
     }
     if (t>2100){
         $(".bz6").css("marginLeft", "-1%");
-        $(".bz6").css("transition", "all .5s linear");
+        $(".bz6").css("transition", "all .8s linear");
     }
     if (t>2400){
         $(".bz7").css("marginLeft", "-1%");
-        $(".bz7").css("transition", "all .5s linear");
+        $(".bz7").css("transition", "all .8s linear");
     }
     if (t>2700){
         $(".bz8").css("marginLeft", "-1%");
-        $(".bz8").css("transition", "all .5s linear");
+        $(".bz8").css("transition", "all .8s linear");
     }
 };
