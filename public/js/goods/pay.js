f8af11dec23c4a63fb6cf8eb18f4e0ed646271f3
YanChunlin96 <1519780292@qq.com>
2018-06-19 10:58:05 +0800

    ycl

--- public/js/goods/pay.js
+++ public/js/goods/pay.js
@@ -2,6 +2,6 @@ setTimeout(function () {
     $("#pay").css("display","none");
     $("#successPay").css("display","block");
     setTimeout(function () {
-        location.href="../index/index";
+        location.href="/index";
     },3000);
 },6000);
