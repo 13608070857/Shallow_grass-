ea2f2a80de058554796f0535e09e252846fd3fd3
jysson <jysson1224@users.noreply.github.com>
2018-06-22 16:56:28 +0800

    s

--- dao/forumDao.js
+++ dao/forumDao.js
@@ -3,7 +3,7 @@ const forumModel = {
     //获取帖子信息
     getForumInfo(){
         return new Promise((resolve,reject)=>{
-            dbpool.connect("SELECT * FROM post WHERE isRecommend='yes'",
+            dbpool.connect("SELECT * FROM post p,forum_category fc WHERE p.categoryId = fc.categoryId and isRecommend='yes'",
                 [],(err,data)=>{
                     if (!err){
                         resolve(data);
