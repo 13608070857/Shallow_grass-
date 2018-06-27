c198cb40f10b8a77d00b5089025a2f29e7ad58f8
YanChunlin96 <1519780292@qq.com>
2018-06-27 16:45:56 +0800

    ycl

--- public/js/goods/goods.js
+++ public/js/goods/goods.js
@@ -2,9 +2,11 @@ $(".goodsDiv").mouseover(function () {
     $(this).children("a").children(".goodsImg").children("img").css("transform","scale(1.2)");
     $(this).children("a").children(".goodsTextDiv").css("transform","translateY(-10px)");
     $(this).css("box-shadow"," 0px 0px 20px 0px #CCC");
+    $(this).children(".collSpan").css("opacity","1");
 });
 $(".goodsDiv").mouseout(function () {
     $(this).children("a").children(".goodsImg").children("img").css("transform","scale(1)");
     $(this).children("a").children(".goodsTextDiv").css("transform","translateY(0)");
     $(this).css("box-shadow","none");
+    $(this).children(".collSpan").css("opacity","0");
 });
